396db87a9816ba4055ac42b46b96abfe88572d94
Linda <linda.tamakaha@gmail.com>
2018-06-20 00:12:40 +0100

    unfinished code for step 6/7

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,9 +1,30 @@
 const Pet = require('../src/pet.js');
 
 describe('constructor', () => {
-    it('returns an object', () => {
-      const pet = new Pet('Fido')
-      pet.growUp();
-      expect(pet.age).toEqual(1);
-    });
+  it('should return an object', () => {
+    expect(new Pet('Fido')).toBeInstanceOf(Object);
   });
+});
+  it('should set the name property', () => {
+    const pet = new Pet('Fido');
+    expect(pet.name).toEqual('Fido');
+  });
+
+  it('should have an initial age of 0', () => {
+    const pet = new Pet('Fido');
+    expect(pet.age).toEqual(0);
+  });
+
+  it('should have an initial hunger of 0 and fitness of 10', () => {
+    const pet = new Pet('Bob');
+    expect(pet.hunger).toEqual(0);
+    expect(pet.fitness).toEqual(10);
+});
+describe('walk', () => {
+  it('increases fitness by 4', () => {
+    const pet = new Pet('fido');
+    pet.fitness = 4;
+    pet.walk();
+    expect(pet.fitness).toEqual(8);
+  });
+});
