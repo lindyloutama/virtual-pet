f37c9df53c4ed671590408a9f9606dd84eddf2a8
Linda <linda.tamakaha@gmail.com>
2018-06-23 00:06:14 +0100

    committing changes

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -2,29 +2,45 @@ const Pet = require('../src/pet.js');
 
 describe('constructor', () => {
   it('should return an object', () => {
-    expect(new Pet('Fido')).toBeInstanceOf(Object);
+    expect(new Pet('Carlos')).toBeInstanceOf(Object);
   });
 });
   it('should set the name property', () => {
-    const pet = new Pet('Fido');
-    expect(pet.name).toEqual('Fido');
+    const pet = new Pet('Carlos');
+    expect(pet.name).toEqual('Carlos');
   });
 
   it('should have an initial age of 0', () => {
-    const pet = new Pet('Fido');
+    const pet = new Pet('Carlos');
     expect(pet.age).toEqual(0);
   });
 
   it('should have an initial hunger of 0 and fitness of 10', () => {
-    const pet = new Pet('Bob');
+    const pet = new Pet('Carlos');
     expect(pet.hunger).toEqual(0);
     expect(pet.fitness).toEqual(10);
 });
 describe('walk', () => {
   it('increases fitness by 4', () => {
-    const pet = new Pet('fido');
+    const pet = new Pet('Carlos');
     pet.fitness = 4;
     pet.walk();
     expect(pet.fitness).toEqual(8);
+  });
+});
+describe('walk', () => {
+  it('increases fitness by to a maximum of 10', () => {
+    const pet = new Pet('Carlos');
+    pet.fitness = 8;
+    pet.walk();
+    expect(pet.fitness).toEqual(10);
+  });
+});
+describe('feed', () => {
+  it('decrease the Pet hunger level by 3', () => {
+    const pet = new Pet('Carlos');
+    pet.hunger = 0;
+    pet.feed();
+    expect(pet.hunger).toEqual(-3);
   });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,11 +1,11 @@
-const MAXIMUM_FITNESS = 10;
+const maxFitness = 10;
+const minHunger = 0;
 
    function Pet (name) {
         this.name = name;
         this.age = 0;
         this.hunger = 0;
         this.fitness = 10;
-
 }
     Pet.prototype.growUp = function() {
         this.age += 1;
@@ -18,5 +18,12 @@ const MAXIMUM_FITNESS = 10;
         } else {
           this.fitness = 10;
         }
-    }
+
+        Pet.prototype.feed = function() {
+            if ((this.hunger - 3) <= minHunger) {
+                this.hunger = 0;
+            } else {
+                this.hunger -= 3;
+            }
+        }
 module.exports = Pet;
